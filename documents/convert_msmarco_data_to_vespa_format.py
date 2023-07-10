f7391378d23620da45139f480480f5209fdc064a
Anna Ruggero <a.ruggero@sease.io>
2023-07-10 13:31:35 +0200

    Updated code

--- documents/convert_msmarco_data_to_vespa_format.py
+++ documents/convert_msmarco_data_to_vespa_format.py
@@ -10,9 +10,18 @@ if __name__ == "__main__":
     count = 1
     for line in input_file.readlines():
         text = line.split("\t")[1]
+        text_cleaned = text.replace("\\d", "d").replace("\\", "")[:-1]
+        paragraphs = text_cleaned.split(".")
+        paragraphs.pop()
         categorical_value = random.randint(0, 7)
         document = document + "{\"put\": \"id:doc:doc::" + str(count) + "\","
-        document = document + "\"fields\": {\"text\": \"" + text.replace("\\d", "d").replace("\\", "")[:-1] + "\","
+        document = document + "\"fields\": {\"text\": \"" + text_cleaned + "\","
+        if len(paragraphs) == 0:
+            paragraphs.append(text_cleaned)
+        document = document + "\"paragraphs\": ["
+        for paragraph in paragraphs:
+            document = document + "\"" + paragraph.replace("\\d", "d").replace("\\", "") + ".\","
+        document = document[:-1] + "],"
         document = document + "\"color\": \"" + categorical_list[categorical_value] + "\"}"
         document = document + "}\n"
         count = count + 1
